1fd598d686ac79d5beac5889a2601919afb91482
ibraim-Ajarmeh <ajarmeh1997@gmail.com>
2019-11-30 23:28:21 +0200

    similar to the papers that given to us at lab 10

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ require('dotenv').config();
 const express = require('express')
 const cors = require('cors')
 const superagent = require('superagent');
+const pg = require('pg');
 
+const client = new pg.Client(process.env.DATABASE_URL);
 const PORT = process.env.PORT || 3000;
-
 const server = express();
 
 server.use(cors());
@@ -17,64 +18,85 @@ server.get('/', (request, response) => {
 
 
 
-server.get('/event', eventHandler);
+server.get('/location', locationHandler);
+server.get('/weather', weatherHandler);
+server.get('/events', eventsHandler);
+server.get('/movies', moviesHandler);
+server.get('/yelp', yelpHandler);
+server.get('/trails', trailsHandler);
 
 
+const client = require('./modules/client.js');
+const location = require('./modules/location.js');
+const weather = require('./modules/weather.js');
+const events = require('./modules/events.js');
+const movies = require('./modules/movies.js');
+const yelp = require('./modules/yelp.js');
+const trails = require('./modules/trails.js');
 
-// ---- Location section ---- \\
+server.use('*', notFoundHandler);
+server.ues(errorHandler);
 
-const location = require('./location.js')
-server.get('/location', location.locationHandler);
-
-
-
-// ---- Weather section ---- \\
-const weather = require('./weather.js')
-server.get('/weather', weather.weatherHandler);
-
-
-// ---- Event section ---- \\
-
-function eventHandler(req, res) {
-    getEvent(req.query.data)
-        .then(eventData => res.status(200).json(eventData));
-
+function locationHandler(req, res) {
+    const city = req.query.data;
+    location.getLoctionDtata(city)
+        .then(data => sendJson(data, res))
+        .catch((error) => errorHndler(error, req, res));
 }
 
-function getEvent(query) {
-    const url = `http://api.eventful.com/json/events/search?app_key=${process.env.EVENT_API_KEY}&location=${query.formatted_query}`;
-    console.log('naseem', url);
-    return superagent.get(url)
-        .then(data => {
-            const eventData = JSON.parse(data.text);
-            return eventData.events.event.map((eventday) => {
-                return new Event(eventday);
-            });
-        });
+function weatherHandler(req, res) {
+    const location = req.query.data;
+    weather(location)
+        .then(summaries => sendJson(summaries, res))
+        .catch((error) => errorHndler(error, req, res));
 }
 
-function Event(day) {
-    this.link = day.url;
-    this.name = day.title;
-    this.event_data = day.start_time;
-    this.summary = day.description;
+function eventsHandler(req, res) {
+    const location = req.query.data.formatted_query;
+    events(location)
+        .then(eventslist => sendJson(eventslist, res))
+        .catch((error) => errorHndler(error, req, res));
+}
+
+function yelpHandler(req, res) {
+    const location = req.query.data.search_query;
+    yelp(location)
+        .then(reviews => sendJson(reviews, res))
+        .catch((error) => errorHndler(error, req, res));
+}
+
+function trailsHandler(req, res) {
+    const latitude = req.query.data.latitude;
+    const longitude = req.query.data.longitude;
+    trails(latitude, longitude)
+        .then(trailslist => sendJson(trailslist, res))
+        .catch((error) => errorHndler(error, req, res));
 }
 
 
+function moviesHandler(req, res) {
+    const location = req.query.data.formatted_query;
+    movies(location)
+        .then(trailslist => sendJson(trailslist, res))
+        .catch((error) => errorHndler(error, req, res));
+}
 
+function sendJson(data, res) {
+    res.status(200).json(data);
+}
 
-server.get('/foo', (request, response) => {
-    throw new Error('ops');
-})
+function notFoundHandler(req, res) {
+    res.status(404).send('what?');
+}
 
-server.use('*', (request, response) => {
-    response.status(404).send('Not Found')
-})
+function errorHandler(error, req, res) {
+    res.status(500).send('error');
+}
 
-server.use((error, request, response) => {
-    response.status(500).send(error)
-})
+function startServer() {
+    server.listen(PORT, () => console.log(`server up on ${PORT}`));
+}
 
-
-
-server.listen(PORT, () => console.log(`app listening on ${PORT}`))
+client.connect()
+    .then(startServer)
+    .catch(err => console.error(err));
